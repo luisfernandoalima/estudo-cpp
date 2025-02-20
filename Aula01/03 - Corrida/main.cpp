e4a2ec074ca71756118a2ed6ec7a1c9e4b2928d8
Luis Lima <luisfernando.lima2005@gmail.com>
2025-02-20 10:20:43 -0300

    Cálculo de gasto de combustível.

---
+++ Aula01/03 - Corrida/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#define GASTO_DO_AUTOMOVEL 12
+using namespace std;
+
+int main() {
+    system("cls");
+    setlocale(LC_ALL, "Portuguese");
+
+    double tempoDaViagem, velocidadeDaViagem;
+
+    cout << "Qual é o tempo gasto da viagem? (Horas) " << endl;
+    cin >> tempoDaViagem;
+
+    cout << "\nQual é a velocidade média durante a viagem? (Km/h) " << endl;
+    cin >> velocidadeDaViagem;
+
+    double distanciaPercorrida = tempoDaViagem * velocidadeDaViagem;
+
+    double litrosUsados = distanciaPercorrida/12;
+
+    cout << "\nVelocidade média: " << velocidadeDaViagem << "Km/h" << endl;
+    cout << "Tempo gasto na viagem: " << tempoDaViagem << " horas" <<endl;
+    cout << "Distancia percorrida: " << distanciaPercorrida << "Km" << endl;
+    cout << "Quantidade de litros utilizados: " << litrosUsados << " Litros\n" << endl;
+
+
+    system("pause");
+    return 0;
+}
